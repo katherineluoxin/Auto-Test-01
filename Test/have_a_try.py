e946c7f188f081b070eb5bde7a011bef9b228ef0
katherineluoxin <121903717@qq.com>
2019-07-03 17:30:25 +0800

    add excelwrite/excelread

--- Test/have_a_try.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-
-driver = webdriver.Chrome()
-url_1 = 'https://www.baidu.com'
-url_2 = 'https://www.douban.com'
-driver.get(url_1)
-driver.execute_script('window.open("%s")' % url_2)
-
--- Test/readme.txt
+++
@@ -1,7 +0,0 @@
-第一次：创建该文档。
-
-第二次：update this file。
-
-第三次：add the third line of words。
-
-第四次：create a new branch that is dev。
